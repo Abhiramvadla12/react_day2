9c8f0dd3a87cfd046fa07fd288d3b77c7dc8227a
Abhiramvadla12 <abhiramvadla61@gmail.com>
2024-12-18 20:23:25 +0530

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from '../components/header'
+import Main from '../components/main_webpage'
+import Footer from '../components/footer'
+function App() {
+  
+
+  return (
+    <>
+     <Header />
+     <Main />
+     <Footer />
+    </>
+  )
+}
+
+export default App
